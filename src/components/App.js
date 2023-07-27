36a562bb615bd2c6d3db56c54b7567317a772c55
Love Gupta <90507983+Lovegupta112@users.noreply.github.com>
2023-07-27 05:20:15 +0000

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,41 @@
 
-import React from "react";
+import React ,{useState} from "react";
 import './../styles/App.css';
 
 const App = () => {
+ 
+  const[input,setInput]=useState('');
+  const[tasks,setTasks]=useState([]);
+
+  console.log('input: ',input);
+  console.log('tasks: ',tasks);
+  function add(){
+    setTasks([...tasks,input]);
+  }
+
+  function deleteTask(task){
+     setTasks(tasks.filter((t)=>{
+          if(t!==task) return t;
+     }))
+  }
+
+
   return (
     <div>
         {/* Do not remove the main div */}
+        <p>To-Do List</p>
+         <div>
+          <input type='text'  value={input} onChange={(e)=>setInput(e.target.value)}/>
+          <button onClick={add}>Add Todo</button>
+         </div>
+         <div>
+           {tasks.length>0 && tasks.map((task,index)=>{
+            return <div style={{display:'flex',justifyContent:'space-between'}} key={index}>
+             <span>{task}</span> 
+             <button onClick={()=>deleteTask(task)}>Delete</button>
+            </div>
+           })}
+         </div>
     </div>
   )
 }
